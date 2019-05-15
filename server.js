1d2d84496ac5ee290c72b5f79eef45708e3b12f6
Luc Patiny <luc@patiny.com>
2019-05-15 22:03:40 +0200

    add debug

--- server.js
+++ server.js
@@ -2,54 +2,50 @@
 const slcan = require('./lib/slcan');
 const frame = require('./lib/frame');
 const app = require('express')();
+const debug = require('debug')('server');
 const ws = require('express-ws')(app);
 
-
-let can = new slcan(process.env.SERIAL,process.env.CANSPEED,process.env.BAUD)
-let alreadyConnected = false
+debug('process.env.SERIAL: ' + process.env.SERIAL);
+debug('process.env.CANSPEED: ' + process.env.CANSPEED);
+debug('process.env.BAUD: ' + process.env.BAUD);
+let can = new slcan(process.env.SERIAL, process.env.CANSPEED, process.env.BAUD);
+let alreadyConnected = false;
 
 app.ws('/', (ws, req) => {
+  if (!alreadyConnected) {
+    console.log('websocket connection');
+    alreadyConnected = true;
 
-  if(!alreadyConnected){
-    console.log('websocket connection')
-    alreadyConnected = true
-
-    can.recvFrameCallback = function(frame){
-      
-      if(process.env.DEBUG){
-        console.log(frame)
+    can.recvFrameCallback = function(frame) {
+      if (process.env.DEBUG) {
+        console.log(frame);
       }
 
-      ws.send(JSON.stringify(frame))
-    }
+      ws.send(JSON.stringify(frame));
+    };
 
-    can.open()  
-  }else{
-    console.log("single user connection allowed only")
+    can.open();
+  } else {
+    console.log('single user connection allowed only');
   }
 
   ws.on('close', function(msg) {
-    can.close()
-    alreadyConnected = false
-  })
-  
-
-  ws.on('message', function(msg) {
-
-    msg = JSON.parse(msg)
-
-    var can_frame = new frame(msg.id);
-    can_frame.data = msg.data
-    can_frame.is_ext_id = msg.is_ext_id
-    can_frame.is_remote = msg.is_remote
-    can_frame.timestamp = msg.timestamp
-    can_frame.dlc = msg.dlc
-
-    can.send(can_frame)
+    can.close();
+    alreadyConnected = false;
   });
 
+  ws.on('message', function(msg) {
+    msg = JSON.parse(msg);
 
+    var can_frame = new frame(msg.id);
+    can_frame.data = msg.data;
+    can_frame.is_ext_id = msg.is_ext_id;
+    can_frame.is_remote = msg.is_remote;
+    can_frame.timestamp = msg.timestamp;
+    can_frame.dlc = msg.dlc;
 
+    can.send(can_frame);
+  });
 });
 
-app.listen(3001, () => console.error('listening on http://localhost:3001/'));
+app.listen(3005, () => console.error('listening on http://localhost:3005/'));
